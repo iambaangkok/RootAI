d8f37ff99b0eaca6f350b55571bc23460bf10de1
Pawaret Dilokwuttisit <pawaret.dilok@gmail.com>
2023-06-27 12:13:11 +0700

    Add colors and ubuntuMonoBold fonts

--- src/config/Colors.py
+++ src/config/Colors.py
@@ -2,3 +2,7 @@ from pygame import Color
 
 GREEN = Color(0, 255, 0, 255)
 WHITE = Color(255, 255, 255, 255)
+
+ORANGE = Color(255, 100, 0)
+BLUE = Color(30, 144, 255)
+GREY = Color(121, 122, 108)
--- src/config/Config.py
+++ src/config/Config.py
@@ -9,3 +9,9 @@ FRAME_RATE = 60
 
 pygame.font.init()
 FONT_1 = Font("../assets/fonts/UbuntuMono-Regular.ttf", 16)
+FONT_MD = Font("../assets/fonts/UbuntuMono-Regular.ttf", 28)
+FONT_MD_BOLD = Font("../assets/fonts/UbuntuMono-Bold.ttf", 28)
+FONT_LG = Font("../assets/fonts/UbuntuMono-Regular.ttf", 32)
+FONT_LG_BOLD = Font("../assets/fonts/UbuntuMono-Bold.ttf", 32)
+FONT_XL = Font("../assets/fonts/UbuntuMono-Regular.ttf", 40)
+FONT_XL_BOLD = Font("../assets/fonts/UbuntuMono-Bold.ttf", 40)
