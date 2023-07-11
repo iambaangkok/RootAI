8f163863c8549deebc62f3db575bc2352ed9cb8f
Baangkok Vanijyananda <iambaangkok@gmail.com>
2023-07-11 16:50:10 +0700

    refactor tuple of 2 into Vector2

--- src/game/Area.py
+++ src/game/Area.py
@@ -86,7 +86,7 @@ class Area:
 
         for i, building in enumerate(self.buildings):
             rect = Rect(0, 0, dimension, dimension)
-            rect.topleft = self.position.__add__(starting_offset).__add__(Vector2(i * dimension + i * gap, 0))
+            rect.topleft = self.position + starting_offset + Vector2(i * dimension + i * gap, 0)
 
             color = Colors.WHITE
             width = 1
@@ -129,7 +129,7 @@ class Area:
         text = ""
 
         offset: Vector2 = Vector2(self.radius - radius)
-        position = self.position.__add__(offset)
+        position = self.position + offset
 
         if self.suit is Suit.MOUSE:
             color = Colors.MOUSE
@@ -159,7 +159,7 @@ class Area:
         text = ""
 
         offset: Vector2 = Vector2(self.radius - radius, -1 * (self.radius - radius))
-        position = self.position.__add__(offset)
+        position = self.position + offset
 
         ruler = self.ruler()
         if ruler is Warrior.MARQUIS:
@@ -196,7 +196,7 @@ class Area:
 
         surface = Config.FONT_SM.render(text, True, color)
         surface_rect = surface.get_rect()
-        surface_rect.center = Rect(self.position.__add__(starting_offset).__add__(Vector2(gap*0, 0)), (0, 0)).center
+        surface_rect.center = Rect(self.position + starting_offset + (Vector2(gap * 0, 0)), (0, 0)).center
 
         screen.blit(surface, surface_rect)
 
@@ -206,7 +206,7 @@ class Area:
 
         surface = Config.FONT_SM.render(text, True, color)
         surface_rect = surface.get_rect()
-        surface_rect.center = Rect(self.position.__add__(starting_offset).__add__(Vector2(gap * 1, 0)), (0, 0)).center
+        surface_rect.center = Rect(self.position + starting_offset + (Vector2(gap * 1, 0)), (0, 0)).center
 
         screen.blit(surface, surface_rect)
 
@@ -220,7 +220,7 @@ class Area:
         if self.token_count[Token.WOOD] > 0:
             rad = math.radians(45)
             offset = Vector2(math.cos(rad) * offset_ratio * self.radius, -1 * math.sin(rad) * offset_ratio * self.radius)
-            position = self.position.__add__(offset)
+            position = self.position + offset
 
             color = Colors.ORANGE
             width = 1
@@ -236,7 +236,7 @@ class Area:
         if self.token_count[Token.CASTLE] > 0:
             rad = math.radians(10)
             offset = Vector2(math.cos(rad) * offset_ratio * self.radius, -1 * math.sin(rad) * offset_ratio * self.radius)
-            position = self.position.__add__(offset)
+            position = self.position + offset
 
             color = Colors.ORANGE
             width = 1
--- src/game/MarquiseBoard.py
+++ src/game/MarquiseBoard.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame import Rect, Color, Surface
+from pygame import Rect, Color, Surface, Vector2
 
 from src.config import Config, Colors
 from src.game.Building import Building
@@ -21,12 +21,8 @@ BUILDING_DRAW_REWARD = [
 BUILDING_TRACKER_NAME = [Building.SAWMILL, Building.WORKSHOP, Building.RECRUITER]
 
 
-def add_tuple(a, b):
-    return tuple(map(lambda i, j: i + j, a, b))
-
-
 class MarquiseBoard:
-    starting_point = (0, 0)
+
     dimension = (0, 0)
 
     BUILDING_TRACKER = {
@@ -36,9 +32,22 @@ class MarquiseBoard:
     }
 
     def __init__(self):
-        self.items = None
         self.name: str = "Marquise de Cat"
         self.color: Color = Colors.ORANGE
+        self.items: {Item: int} = {
+            Item.KEG: 5,
+            Item.BAG: 7,
+            Item.KNIFE: 4,
+            Item.BOOTS: 3,
+            Item.HAMMER: 2,
+            Item.CROSSBOW: 1,
+            Item.COIN: 0,
+            Item.TORCH: 3,
+        }
+        self.card_list: list[int] = [1, 55, 32, 2, 4, 15, 25, 35, 45, 99, 66, 22, 3, 5, 5, 2, 54, 8, 9, 58, 48, 2, 82, 98]
+        self.reserved_warriors: int = 24
+
+        self.starting_point: Vector2 = Vector2(0, 0)
 
     def draw(self, screen: Surface):
         self.dimension = (screen.get_width() * 0.25, screen.get_height() * 0.5)
@@ -47,28 +56,28 @@ class MarquiseBoard:
 
         # Text
         points_text = Config.FONT_MD_BOLD.render("Marquise de Cat", True, Colors.ORANGE)
-        shift = (10, 10)
+        shift: Vector2 = Vector2(10, 10)
 
-        screen.blit(points_text, add_tuple(self.starting_point, shift))
+        screen.blit(points_text, self.starting_point + shift)
 
         for i in range(3):
-            self.draw_tracker(screen, BUILDING_TRACKER_NAME[i], add_tuple(self.starting_point, (5, 45 * i + points_text.get_height() + 20))
+            self.draw_tracker(screen, BUILDING_TRACKER_NAME[i], self.starting_point + Vector2(5, 45 * i + points_text.get_height() + 20)
                               )
 
-        self.draw_crafted_items(screen, add_tuple(self.starting_point, (5, 45 * 3 + points_text.get_height() + 20)))
-        self.draw_crafted_cards(screen, add_tuple(self.starting_point, (5, 45 * 5 + points_text.get_height() + 20)))
-        self.draw_warriors_reserve(screen, add_tuple(self.starting_point, (5, 45 * 6 + 25 + points_text.get_height() + 20)))
-        self.draw_cards_in_hand(screen, add_tuple(self.starting_point, (5, 45 * 6 + 50 + points_text.get_height() + 20)))
+        self.draw_crafted_items(screen, self.starting_point + Vector2(5, 45 * 3 + points_text.get_height() + 20))
+        self.draw_crafted_cards(screen, self.starting_point + Vector2(5, 45 * 5 + points_text.get_height() + 20))
+        self.draw_warriors_reserve(screen, self.starting_point + Vector2(5, 45 * 6 + 25 + points_text.get_height() + 20))
+        self.draw_cards_in_hand(screen, self.starting_point + Vector2(5, 45 * 6 + 50 + points_text.get_height() + 20))
 
-    def draw_tracker(self, screen, title, starting_point):
+    def draw_tracker(self, screen: Surface, title: Building, starting_point: Vector2):
 
         # Text
         title_text = Config.FONT_SM_BOLD.render(str(title), True, Colors.ORANGE)
-        shift = (10, 10)
+        shift: Vector2 = Vector2(10, 10)
 
-        screen.blit(title_text, add_tuple(starting_point, shift))
+        screen.blit(title_text, starting_point + shift)
 
-        img_size = (40, 40)
+        img_size: Vector2 = Vector2(40, 40)
 
         alpha = 200
         img = pygame.image.load("../assets/images/marquise/{}.png".format(title))
@@ -84,33 +93,22 @@ class MarquiseBoard:
             else:
                 draw_img = img
             screen.blit(draw_img,
-                        (starting_point[0] + (img_size[0] + 10) * j + 10 + 100, starting_point[1]))
+                        (starting_point.x + (img_size.x + 10) * j + 10 + 100, starting_point.y))
 
             reward = Config.FONT_SM_BOLD.render("+" + str(BUILDING_REWARD[title][j]), True, (206, 215, 132))
             reward = text.add_outline_to_image(reward, 2, Colors.GREY_DARK_2)
 
-            screen.blit(reward, (starting_point[0] + (img_size[0] + 10) * j + 10 + 100, starting_point[1]))
+            screen.blit(reward, (starting_point.x + (img_size.x + 10) * j + 10 + 100, starting_point.y))
 
-    def draw_crafted_items(self, screen, starting_point):
-
-        self.items = {
-            Item.KEG: 5,
-            Item.BAG: 7,
-            Item.KNIFE: 4,
-            Item.BOOTS: 3,
-            Item.HAMMER: 2,
-            Item.CROSSBOW: 1,
-            Item.COIN: 0,
-            Item.TORCH: 3,
-        }
+    def draw_crafted_items(self, screen: Surface, starting_point: Vector2):
 
         # Text
         title_text = Config.FONT_SM_BOLD.render("Crafted Items", True, Colors.ORANGE)
-        shift = (10, 10)
+        shift: Vector2 = Vector2(10, 10)
 
-        screen.blit(title_text, add_tuple(starting_point, shift))
+        screen.blit(title_text, starting_point + shift)
 
-        img_size = (40, 40)
+        img_size: Vector2 = Vector2(40, 40)
 
         ind = 0
         for key in self.items:
@@ -122,23 +120,22 @@ class MarquiseBoard:
             img = pygame.transform.scale(img, img_size)
 
             screen.blit(img,
-                        (starting_point[0] + (img_size[0] + 10) * col + 10 + 150, starting_point[1] + (img_size[0] + 5) * row))
+                        (starting_point.x + (img_size.x + 10) * col + 10 + 150, starting_point.y + (img_size.x + 5) * row))
 
             quantity = Config.FONT_SM_BOLD.render("x{}".format(value), True, (206, 215, 132))
             quantity = text.add_outline_to_image(quantity, 2, Colors.GREY_DARK_2)
-            screen.blit(quantity, (starting_point[0] + (img_size[0] + 10) * col + 10 + 150, starting_point[1] + (img_size[0] + 5) * row))
+            screen.blit(quantity, (starting_point.x + (img_size.x + 10) * col + 10 + 150, starting_point.y + (img_size.x + 5) * row))
             ind = ind + 1
 
-    def draw_crafted_cards(self, screen, starting_point):
+    def draw_crafted_cards(self, screen: Surface, starting_point: Vector2):
 
-        self.card_list = [1, 55, 32, 2, 4, 15, 25, 35, 45, 99, 66, 22, 3, 5, 5, 2, 54, 8, 9, 58, 48, 2, 82, 98]
         # Text
         title_text = Config.FONT_SM_BOLD.render("Crafted Cards", True, Colors.ORANGE)
-        shift = (10, 10)
+        shift: Vector2 = Vector2(10, 10)
 
-        screen.blit(title_text, add_tuple(starting_point, shift))
+        screen.blit(title_text, starting_point + shift)
 
-        block_size = (20, 20)
+        block_size: Vector2 = Vector2(20, 20)
 
         ind = 0
 
@@ -148,20 +145,20 @@ class MarquiseBoard:
 
             card_ind = Config.FONT_SM_BOLD.render('{0:02d}'.format(key), True, (206, 215, 132))
             card_ind = text.add_outline_to_image(card_ind, 2, Colors.GREY_DARK_2)
-            screen.blit(card_ind, (starting_point[0] + (block_size[0] + 10) * col + 10 + 150, starting_point[1] + (block_size[0] + 5) * row))
+            screen.blit(card_ind, (starting_point.x + (block_size.x + 10) * col + 10 + 150, starting_point.y + (block_size.x + 5) * row))
             ind = ind + 1
         pass
 
-    def draw_warriors_reserve(self, screen, starting_point):
-        title_text = Config.FONT_SM_BOLD.render("Reserved Warriors: {}".format(69), True, Colors.ORANGE)
-        shift = (10, 10)
-        screen.blit(title_text, add_tuple(starting_point, shift))
+    def draw_warriors_reserve(self, screen: Surface, starting_point: Vector2):
+        title_text = Config.FONT_SM_BOLD.render("Reserved Warriors: {}".format(self.reserved_warriors), True, Colors.ORANGE)
+        shift: Vector2 = Vector2(10, 10)
+        screen.blit(title_text, starting_point + shift)
 
         pass
 
-    def draw_cards_in_hand(self, screen, starting_point):
-        title_text = Config.FONT_SM_BOLD.render("Cards in Hand: {}".format(69), True, Colors.ORANGE)
-        shift = (10, 10)
-        screen.blit(title_text, add_tuple(starting_point, shift))
+    def draw_cards_in_hand(self, screen: Surface, starting_point: Vector2):
+        title_text = Config.FONT_SM_BOLD.render("Cards in Hand: {}".format(len(self.card_list)), True, Colors.ORANGE)
+        shift: Vector2 = Vector2(10, 10)
+        screen.blit(title_text, starting_point + shift)
 
         pass
